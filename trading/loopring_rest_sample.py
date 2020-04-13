843fb6d4d59af610f66224eb6f18d4efa6737221
yue.wang <yue.wang@loopring>
2020-04-13 15:02:59 +0800

    fix review comments
    Signed-off-by: yue.wang <yue.wang@loopring>

--- trading/loopring_rest_sample.py
+++ trading/loopring_rest_sample.py
@@ -198,18 +198,23 @@ class LoopringRestApiSample(RestClient):
         """
         self._order(base_token, quote_token, False, price, volume)
 
-    def _order(self, base_token, quota_token, buy, price, volume):
-        contractS = self.market_info_map[base_token]
-        contractB = self.market_info_map[quota_token]
-        amountS = str(int(10 ** contractS['decimals'] * volume))
-        amountB = str(int(10 ** contractB['decimals'] * price * volume))
-        tokenSId = contractS['tokenId']
-        tokenBId = contractB['tokenId']
+    def _order(self, base_token, quote_token, buy, price, volume):
         if buy:
-            tokenSId, tokenBId = tokenBId, tokenSId
-            amountS, amountB = amountB, amountS
+            tokenS = self.market_info_map[quote_token]
+            tokenB = self.market_info_map[base_token]
+            amountS = str(int(10 ** tokenS['decimals'] * price * volume))
+            amountB = str(int(10 ** tokenB['decimals'] * volume))
+        else:
+            tokenS = self.market_info_map[base_token]
+            tokenB = self.market_info_map[quote_token]
+            amountS = str(int(10 ** tokenS['decimals'] * volume))
+            amountB = str(int(10 ** tokenB['decimals'] * price * volume))
+
+        tokenSId = tokenS['tokenId']
+        tokenBId = tokenB['tokenId']
 
         orderId = self.orderId[tokenSId]
+        assert orderId < self.MAX_ORDER_ID
         self.orderId[tokenSId] += 1
 
         # make valid time ahead 1 hour
@@ -292,7 +297,7 @@ class LoopringRestApiSample(RestClient):
         if "orderHash" in cancel_params:
             params["orderHash"] = cancel_params["orderHash"]
         if "orderid" in cancel_params:
-            params["clientOrderId"] = cancel_params["orderid"]
+            params["clientOrderId"] = cancel_params["clientOrderId"]
 
         print(f"cancel_order {params}")
         self.add_request(
